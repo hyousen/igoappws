cd2f758ea8d7943fda70a3486b18c31a7819bdcd
hyousen <mathegg201157@gmail.com>
2021-06-27 13:47:17 +0900

    初期化するとのif文を少なくした

--- board.rb
+++ board.rb
@@ -1,4 +1,4 @@
-﻿require 'gosu'
+﻿ require 'gosu'
 
 IMAGE_SIZE = 32
 OUTSIDE_BOARD = -1
@@ -19,13 +19,12 @@ class Board
 	def initialize
 		
 		@board = Array.new(TABLE_SIZE){ Array.new(TABLE_SIZE,0)}
-		@board.each_with_index{ |row,i|
-			row.each_with_index {|column,j|
-				if i == 0 || i == (TABLE_SIZE - 1) || j == 0 || j == (TABLE_SIZE - 1) then
-					@board[i][j] = OUTSIDE_BOARD
-				end
-			}
-		}
+		TABLE_SIZE.times do |i|
+			@board[0][i] = OUTSIDE_BOARD
+			@board[TABLE_SIZE - 1][i] = OUTSIDE_BOARD
+			@board[i][0] = OUTSIDE_BOARD
+			@board[i][TABLE_SIZE - 1] = OUTSIDE_BOARD
+		end
 
 		@tekazu = 1
 
@@ -42,12 +41,15 @@ class Board
 				end
 			end
 		end
+
 		if @board[@a][@b] == BLANK_POINT then
 			if isBlack(@tekazu) then
 				@board[@a][@b] = BLACK_STONE
 			else
 				@board[@a][@b] = WHITE_STONE
 			end
+			
+			
 			addTekazu
 		end
 	end
