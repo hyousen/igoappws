88b5dc15aa59c363d7ec81ed45c7e59087a0132a
K-Shimogai <shimogai_koji@shitamiyabiikoujinoMacBook-Pro.local>
2021-07-11 22:59:39 +0900

    不要な設定の除去と、boardの大きさをconfigとして保持

--- Applications/igoapp.rb
+++ Applications/igoapp.rb
@@ -1,29 +1,26 @@
-﻿ require 'gosu'
-require './board.rb'
-
-TABLE_SIZE = 11
-WIDTH_MARGIN = 0
+﻿require 'gosu'
+require './domain/entity/game_config'
+require './domain/entity/board'
 
 class Igo < Gosu::Window
+  def initialize
+    super 480, 480
+    self.caption = '囲碁アプリ'
+    @config = GameConfig.new(2, 0, 11, 500)
+    @board = Board.new(@config.board_size)
+  end
 
-	def initialize
-		super 480, 480
-		self.caption = "囲碁アプリ"
-		@board = Board.new
-  	end
+  def update
+    if Gosu.button_down?(Gosu::MsLeft)
+      x = mouse_x
+      y = mouse_y
+      @board.place(x, y)
+    end
+  end
 
-	def update
-		if Gosu::button_down?(Gosu::MsLeft)
-			x = mouse_x
-			y = mouse_y
-			@board.place(x,y)
-		end
-	end
-
-	def draw
-		@board.draw
-	end
-
+  def draw
+    @board.draw
+  end
 end
 
 Igo.new.show
