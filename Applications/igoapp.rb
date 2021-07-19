2a9cf9234a5653f36b0e9682a275178f086d5628
K-Shimogai <shimogai_koji@shitamiyabiikoujinoMacBook-Pro.local>
2021-07-19 22:18:48 +0900

    PlayerActionを用いてupdateメソッドを再定義

--- Applications/igoapp.rb
+++ Applications/igoapp.rb
@@ -2,21 +2,31 @@
 require './domain/entity/game_config'
 require './domain/entity/board'
 require './service/drawer/board_drawer'
+require './domain/entity/player'
+require './service/player_action'
+require './domain/value_object/board_position'
+require './domain/entity/game_record'
 
 class Igo < Gosu::Window
   def initialize
     super 480, 480
     self.caption = '囲碁アプリ'
     @config = GameConfig.new(2, 0, 11, 500)
+    # @Todo configのプレイヤー人数による場合わけ
+    @player1 = Player.first('human', 'プレイヤー1')
+    @player2 = Player.second('human', 'プレイヤー2')
     @board = Board.new(@config.board_size)
   end
 
   def update
-    if Gosu.button_down?(Gosu::MsLeft)
-      x = mouse_x
-      y = mouse_y
-      @board.place(x, y)
-    end
+    return unless Gosu.button_down?(Gosu::MsLeft)
+
+    # @FixMe board_positionはmoveに集約する?
+    board_position = BoardPosition.new(@board, mouse_x, mouse_y)
+    next_tekazu = GameRecord.tekazu + 1
+    now_player = next_tekazu.odd? ? @player1 : @player2
+    player_action = PlayerAction.new
+    player_action.place_stone(@board, now_player, board_position)
   end
 
   def draw
