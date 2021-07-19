3fa0251e16e8c283680eb5dc06c1f0f699627668
K-Shimogai <shimogai_koji@shitamiyabiikoujinoMacBook-Pro.local>
2021-07-19 22:11:57 +0900

    プレイヤーの手を表すMoveクラスを実装

---
+++ Applications/domain/value_object/stone.rb
@@ -0,0 +1,27 @@
+# Moveクラスに集約?
+class Stone
+  FIRST_PLAYER = 'Black'
+  SECOND_PLAYER = 'White'
+
+  private
+
+  def initialize(stone_color)
+    @color = stone_color
+  end
+
+  public
+
+  def self.black
+    Stone.new(Stone::FIRST_PLAYER)
+  end
+
+  def self.white
+    Stone.new(Stone::SECOND_PLAYER)
+  end
+
+  attr_reader :color
+
+  def ==(other)
+    @color == other.color
+  end
+end
