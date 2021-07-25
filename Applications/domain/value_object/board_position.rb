763bbd373e9ea507980de4ba230dc45828f67228
K-Shimogai <shimogai_koji@shitamiyabiikoujinoMacBook-Pro.local>
2021-07-25 18:34:56 +0900

    board_positionクラスの不要な属性を削除し、==演算を改善

--- Applications/domain/value_object/board_position.rb
+++ Applications/domain/value_object/board_position.rb
@@ -2,29 +2,31 @@ require_relative '../../module/board_image_config'
 
 # @FixMe 複雑なので factory に切り出すべき？
 # @FixMe Moveクラスに集約?
+# @FixMe バリデーション
 class BoardPosition
   include BoardImageConfig
   def initialize(board, point_x, point_y)
-    # 後に使わなかったら@point_xと@point_yは消す予定
-    @point_x = point_x
-    @point_y = point_y
-    @table_row = table_num(board.size, point_x)
-    @table_col = table_num(board.size, point_y)
+    board_size = board.size
+    @table_row = table_num(board_size, point_x)
+    @table_col = table_num(board_size, point_y)
   end
 
   private
 
   def table_num(board_size, dec)
     board_size.times do |int|
-      return int if dec >= int * BoardImageConfig::BOARD_IMAGE_SIZE && dec < (int + 1) * BoardImageConfig::BOARD_IMAGE_SIZE
+      if dec >= int * BoardImageConfig::BOARD_IMAGE_SIZE \
+        && dec < (int + 1) * BoardImageConfig::BOARD_IMAGE_SIZE
+        return int
+      end
     end
   end
 
   public
 
-  attr_reader :row, :col, :table_row, :table_col
+  attr_reader :table_row, :table_col
 
   def ==(other)
-    @row == other.row && @col == other.col
+    @table_row == other.table_row && @table_col == other.table_col
   end
 end
